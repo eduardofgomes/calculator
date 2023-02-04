7ae15094423a4a53628be27159a0899db19e13c7
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-02-03 23:32:17 -0300

    javascript changes

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ var operationButtons = document.querySelectorAll("#taltaltal")
 function calculator([buttons]) {
     
 }
+
+function insert(number) {
+    var anotherNumber = document.querySelector("#resultado").innerHTML;
+    document.querySelector("#resultado").innerHTML = number + anotherNumber;
+}
+
+function clean() {
+    document.querySelector("#resultado").innerHTML = "";
+}
