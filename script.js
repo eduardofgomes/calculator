78a92d4cea02503bbe6acaccc905fb346c3aa6f1
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-02-04 00:17:00 -0300

    make functions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var buttons = document.querySelectorAll(".botao")
-var operationButtons = document.querySelectorAll("#taltaltal")
-
-function calculator([buttons]) {
-    
-}
-
 function insert(number) {
-    var anotherNumber = document.querySelector("#resultado").innerHTML;
-    document.querySelector("#resultado").innerHTML = number + anotherNumber;
+    
+    var anotherNumber = document.querySelector("#resultado").innerHTML
+    document.querySelector("#resultado").innerHTML = number + anotherNumber
 }
 
 function clean() {
-    document.querySelector("#resultado").innerHTML = "";
+    document.querySelector("#resultado").innerHTML = ""
+}
+
+function back() {
+    let resultado = document.querySelector("#resultado")
+    let array = [...resultado].pop()
+    resultado.innerHTML = array
 }
