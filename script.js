f0c52ee79288100d68b41a66feb584c1ffe69396
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-02-06 20:05:41 -0300

    Alterations of script of the project

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandText) {
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandText = previousOperandText;
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear() {
@@ -20,7 +21,13 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
@@ -29,6 +36,7 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -36,7 +44,7 @@ const numberButtons = document.querySelectorAll('[data-number')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const allCLearButton = document.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -54,4 +62,9 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
 })
